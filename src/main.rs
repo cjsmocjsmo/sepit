1a8964ab492a294870229827ba6a06482064885c
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-11-17 00:40:50 -0800

    Refactored image and video file organization

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,18 @@ use md5::compute;
 use walkdir::WalkDir;
 fn main() {
     let img_path = "/media/pipi/0123-4567/Images/";
-    // for e in WalkDir::new(img_path)
-    //     .follow_links(true)
-    //     .into_iter()
-    //     .filter_map(|e| e.ok())
-    // {
-    //     if e.metadata().unwrap().is_file() {
-    //         let fname = e.path();
-    //         let _new_filename = sanitize_filename(fname);
-    //     }
-    // };
 
-    let jpg_path = "/media/pipi/0123-4567/Images/jpg/";
-    let png_path = "/media/pipi/0123-4567/Images/png/";
-    let gif_path = "/media/pipi/0123-4567/Images/gif/";
-    let bmp_path = "/media/pipi/0123-4567/Images/bmp/";
-    let tif_path = "/media/pipi/0123-4567/Images/tif/";
-    let mov_path = "/media/pipi/0123-4567/AV/";
-    let paths_list = vec![jpg_path, png_path, gif_path, bmp_path, tif_path, mov_path];
-    let _zlist = paths_list.iter().map(|x| std::fs::create_dir_all(x)).collect::<Vec<_>>();
+    let paths_list = vec![
+        "/media/pipi/0123-4567/Images/jpg/",
+        "/media/pipi/0123-4567/Images/png/",
+        "/media/pipi/0123-4567/Images/bmp/",
+        "/media/pipi/0123-4567/Images/tif/",
+        "/media/pipi/0123-4567/AV/",
+    ];
+    let _zlist = paths_list
+        .iter()
+        .map(|x| std::fs::create_dir_all(x))
+        .collect::<Vec<_>>();
 
     let mlist = ["avi", "AVI", "mpg", "MPG", "mp4", "MP4"];
 
@@ -34,43 +27,59 @@ fn main() {
     {
         if e.metadata().unwrap().is_file() {
             let fname = e.path().to_string_lossy().to_string();
-            println!("fname: {}", fname);
+            println!("Processing: {}", fname);
             let digest = compute(fname.clone());
             let parts = &fname.split(".").collect::<Vec<&str>>();
             let ext = parts.last().unwrap();
             if ext == &"jpg" || ext == &"JPG" || ext == &"jpeg" || ext == &"JPEG" {
                 let new_jpg_addr = format!("{:?}", digest) + ".jpg";
-                let jpg_rename_resp = std::fs::rename(fname.clone(), jpg_path.to_owned() + &new_jpg_addr + ".jpg");
+                let jpg_rename_resp =
+                    std::fs::rename(fname.clone(), paths_list[0].to_owned() + &new_jpg_addr + ".jpg");
                 match jpg_rename_resp {
                     Ok(_) => println!("{} moved to {}", fname, new_jpg_addr),
-                    Err(e) => {std::fs::remove_file(fname.clone()).unwrap(); println!("Error {} not moved so removing {}", e, fname)}
+                    Err(e) => {
+                        std::fs::remove_file(fname.clone()).unwrap();
+                        println!("Error {} not moved so removing {}", e, fname)
+                    }
                 }
             } else if ext == &"png" || ext == &"PNG" {
                 let new_png_addr = format!("{:?}", digest) + ".png";
-                let png_rename_resp = std::fs::rename(fname.clone(), png_path.to_owned() + &new_png_addr + ".png");
+                let png_rename_resp =
+                    std::fs::rename(fname.clone(), paths_list[1].to_owned() + &new_png_addr + ".png");
                 match png_rename_resp {
                     Ok(_) => println!("{} moved to {}", fname, new_png_addr),
-                    Err(e) => {std::fs::remove_file(fname.clone()).unwrap(); println!("Error {} not moved so removing {}", e, fname)},
+                    Err(e) => {
+                        std::fs::remove_file(fname.clone()).unwrap();
+                        println!("Error {} not moved so removing {}", e, fname)
+                    }
                 }
             } else if ext == &"bmp" || ext == &"BMP" {
                 let new_bmp_addr = format!("{:?}", digest) + ".bmp";
-                let bmp_rename_resp = std::fs::rename(fname.clone(), bmp_path.to_owned() + &new_bmp_addr + ".bmp");
+                let bmp_rename_resp =
+                    std::fs::rename(fname.clone(), paths_list[2].to_owned() + &new_bmp_addr + ".bmp");
                 match bmp_rename_resp {
                     Ok(_) => println!("{} moved to {}", fname, new_bmp_addr),
-                    Err(e) => {std::fs::remove_file(fname.clone()).unwrap(); println!("Error {} not moved so removing {}", e, fname)},
+                    Err(e) => {
+                        std::fs::remove_file(fname.clone()).unwrap();
+                        println!("Error {} not moved so removing {}", e, fname)
+                    }
                 }
             } else if ext == &"tif" || ext == &"TIF" || ext == &"tiff" || ext == &"TIFF" {
                 let new_tif_addr = format!("{:?}", digest) + ".tif";
-                let tif_rename_results = std::fs::rename(fname.clone(), tif_path.to_owned() + &new_tif_addr + ".tif");
+                let tif_rename_results =
+                    std::fs::rename(fname.clone(), paths_list[3].to_owned() + &new_tif_addr + ".tif");
                 match tif_rename_results {
                     Ok(_) => println!("{} moved to {}", fname, new_tif_addr),
-                    Err(e) => {std::fs::remove_file(fname.clone()).unwrap(); println!("Error {} not moved so removing {}", e, fname)},
+                    Err(e) => {
+                        std::fs::remove_file(fname.clone()).unwrap();
+                        println!("Error {} not moved so removing {}", e, fname)
+                    }
                 }
             } else if mlist.contains(&ext) {
                 let new_mov_addr = format!("{:?}", digest);
                 std::fs::rename(
                     fname.clone(),
-                    mov_path.to_owned() + &new_mov_addr + "." + ext.to_lowercase().as_str(),
+                    paths_list[4].to_owned() + &new_mov_addr + "." + ext.to_lowercase().as_str(),
                 )
                 .unwrap();
             }
